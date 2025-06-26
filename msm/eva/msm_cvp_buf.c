29d6a081bfb8e485f0ce97fb4a1d20aa740ee348
Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>
2025-06-26 16:43:28 +0530

    msm: eva: Fix for smem ref count leak for persist buffers
    Make sure to remove the persist buffer entry from the list
    if the session send commmand fails after buffer map succeed.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2025-27039
    CRs-Fixed: 3878576
    
    Change-Id: I6b90c667a35f662e27755950e529de3c921d8484
    Signed-off-by: Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>
    Signed-off-by: Abishek K V <abishekk@motorola.com>
    Reviewed-on: https://gerrit.mot.com/3450386
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Chakradhar Gajjala <gajjalac@motorola.com>
    Reviewed-by: Varun Shrivastava <varunshrivastava@motorola.com>
    Reviewed-by: Yinjun Chen <a7301c@motorola.com>
    Submit-Approved: Jira Key

--- msm/eva/msm_cvp_buf.c
+++ msm/eva/msm_cvp_buf.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/pid.h>
@@ -1527,7 +1528,7 @@ int msm_cvp_mark_user_persist(struct msm_cvp_inst *inst,
 
 int msm_cvp_map_user_persist(struct msm_cvp_inst *inst,
 			struct eva_kmd_hfi_packet *in_pkt,
-			unsigned int offset, unsigned int buf_num)
+			unsigned int offset, unsigned int buf_num, uint32_t *fd_arr)
 {
 	struct cvp_buf_type *buf;
 	struct cvp_hfi_cmd_session_hdr *cmd_hdr;
@@ -1554,6 +1555,7 @@ int msm_cvp_map_user_persist(struct msm_cvp_inst *inst,
 
 			return -EINVAL;
 		}
+		fd_arr[i] = buf->fd;
 		buf->fd = iova;
 	}
 	return 0;
