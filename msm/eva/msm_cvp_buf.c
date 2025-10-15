3c7d78cb431230fb9489ccea5d57a2e5f2b32e51
Abishek K V <abishekk@motorola.com>
2025-10-15 22:52:39 +0530

    Revert "msm: eva: Fix for smem ref count leak for persist buffers"
    This reverts commit b39bd2c3d3dc192dc1b087f521b07516f820dfae.
    
    Reason for revert: The b39bd2c3d3dc192dc1b087f521b07516f820dfae commit is for
    eva-kernel.lnx.1.0 branch which is not supported on current branch eva-kernel.lnx.2.0
    
    Change-Id: I7fe823e532c6e24b02c8d0cec61918bea843deb4
    Reviewed-on: https://gerrit.mot.com/3594223
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Obi-Wan Kenobi
    Reviewed-by: Yinjun Chen <a7301c@motorola.com>
    Tested-by: Navya Vijayan <navya@motorola.com>
    Tested-by: Jira Key
    Submit-Approved: Jira Key
    (cherry picked from commit 7a07dc0c964b281f1469c50b14cfd782510d6ee0)

--- msm/eva/msm_cvp_buf.c
+++ msm/eva/msm_cvp_buf.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/pid.h>
@@ -1528,7 +1527,7 @@ int msm_cvp_mark_user_persist(struct msm_cvp_inst *inst,
 
 int msm_cvp_map_user_persist(struct msm_cvp_inst *inst,
 			struct eva_kmd_hfi_packet *in_pkt,
-			unsigned int offset, unsigned int buf_num, uint32_t *fd_arr)
+			unsigned int offset, unsigned int buf_num)
 {
 	struct cvp_buf_type *buf;
 	struct cvp_hfi_cmd_session_hdr *cmd_hdr;
@@ -1555,7 +1554,6 @@ int msm_cvp_map_user_persist(struct msm_cvp_inst *inst,
 
 			return -EINVAL;
 		}
-		fd_arr[i] = buf->fd;
 		buf->fd = iova;
 	}
 	return 0;
--- msm/eva/msm_cvp_buf.h
+++ msm/eva/msm_cvp_buf.h
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #ifndef _MSM_CVP_BUF_H_
@@ -212,7 +211,7 @@ int msm_cvp_mark_user_persist(struct msm_cvp_inst *inst,
 			unsigned int offset, unsigned int buf_num);
 int msm_cvp_map_user_persist(struct msm_cvp_inst *inst,
 			struct eva_kmd_hfi_packet *in_pkt,
-			unsigned int offset, unsigned int buf_num, uint32_t *fd_arr);
+			unsigned int offset, unsigned int buf_num);
 int msm_cvp_map_frame(struct msm_cvp_inst *inst,
 		struct eva_kmd_hfi_packet *in_pkt,
 		unsigned int offset, unsigned int buf_num);
